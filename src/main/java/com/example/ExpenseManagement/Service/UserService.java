17f73ea856e8dfd619dba1dd86b2bf758b4d382d
anhthu100903 <anhthu115599@gmail.com>
2025-03-31 16:49:19 +0700

    aaa

--- src/main/java/com/example/ExpenseManagement/Service/UserService.java
+++ src/main/java/com/example/ExpenseManagement/Service/UserService.java
@@ -4,6 +4,6 @@ import com.example.ExpenseManagement.DTO.Response.UserResponse;
 
 public interface UserService {
 	public UserResponse getUserById(Integer id);
-//	public UserResponse getUserByUsername(String username);
-//	public UserResponse getUserByEmail(String email);
+	public UserResponse getUserByUsername(String username);
+	public UserResponse getUserByEmail(String email);
 }
